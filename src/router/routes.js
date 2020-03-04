2b34aca364f1959a0483ae34b757fb9e1988d9b2
phojie rengel <phojrengel@gmail.com>
2020-03-05 02:43:15 +0800

    Test

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,78 +1,78 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/DashboardLayout.vue'),
+    path: "/",
+    component: () => import("layouts/DashboardLayout.vue"),
     meta: { authRequired: true },
     children: [
       {
-        path: '/',
-        name: 'dashboard',
-        component: () => import('pages/dashboard.vue')
+        path: "/",
+        name: "dashboard",
+        component: () => import("pages/dashboard.vue")
       }
     ]
   },
   {
-    path: '/eventId/:eventId',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/eventId/:eventId",
+    component: () => import("layouts/MainLayout.vue"),
     meta: { authRequired: true },
     children: [
       {
-        path: '',
-        name: 'eventDash',
-        component: () => import('pages/overview.vue')
+        path: "",
+        name: "eventDash",
+        //overview
+        component: () => import("pages/score.vue")
       },
       {
-        path: 'contestants',
-        name: 'contestants',
-        component: () => import('pages/contestants.vue')
+        path: "contestants",
+        name: "contestants",
+        component: () => import("pages/contestants.vue")
       },
       {
-        path: 'criteria',
-        name: 'criteria',
-        component: () => import('pages/criteria.vue')
+        path: "criteria",
+        name: "criteria",
+        component: () => import("pages/criteria.vue")
       },
       {
-        path: 'judges',
-        name: 'judges',
-        component: () => import('pages/judges.vue')
+        path: "judges",
+        name: "judges",
+        component: () => import("pages/judges.vue")
       },
       {
-        path: 'score',
-        name: 'score',
-        component: () => import('pages/score.vue')
+        path: "score",
+        name: "score",
+        component: () => import("pages/score.vue")
       },
       {
-        path: 'organizer',
-        name: 'organizer',
-        component: () => import('pages/organizer.vue')
+        path: "organizer",
+        name: "organizer",
+        component: () => import("pages/organizer.vue")
       },
       {
-        path: 'reports',
-        name: 'reports',
-        component: () => import('pages/reports.vue')
+        path: "reports",
+        name: "reports",
+        component: () => import("pages/reports.vue")
       },
       {
-        path: 'settings',
-        name: 'eventSettings',
-        component: () => import('pages/settings.vue')
+        path: "settings",
+        name: "eventSettings",
+        component: () => import("pages/settings.vue")
       }
     ]
   },
   {
-    path: '/login',
-    name: 'login',
+    path: "/login",
+    name: "login",
     meta: { loginRequired: true },
-    component: () => import('pages/Auth/login.vue')
+    component: () => import("pages/Auth/login.vue")
   }
-]
+];
 
 // Always leave this as last one
-if (process.env.MODE !== 'ssr') {
+if (process.env.MODE !== "ssr") {
   routes.push({
-    path: '*',
-    component: () => import('pages/Error404.vue')
-  })
+    path: "*",
+    component: () => import("pages/Error404.vue")
+  });
 }
 
-export default routes
+export default routes;
